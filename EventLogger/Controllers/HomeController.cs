12d346fce4c545e2000a7585b2d2d43beac7b4d8
An Vu <aqv1321@jagmail.southalabama.edu>
2018-04-30 20:17:03 -0500

    1

--- EventLogger/Controllers/HomeController.cs
+++ EventLogger/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EventLogger.Models;
 
 namespace EventLogger.Controllers
 {
@@ -18,7 +19,8 @@ namespace EventLogger.Controllers
         public ActionResult EventTypes()
         {
             ViewBag.Title = "";
-            return View("EventTypes");
+            using(var context = new EventLoggerDataContext())
+            return View("EventTypes", context.EventTypes.ToList());
         }
     }
 }
